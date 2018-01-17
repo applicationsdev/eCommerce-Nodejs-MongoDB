4b19ff3f90d41e49d8eb858203f26c6adb8f8d7a
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-01-17 15:08:31 +0000

    User model

---
+++ models/user.js
@@ -0,0 +1,48 @@
+/* Requires */
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var crypto = require('crypto');
+var schema = mongoose.Schema;
+
+/* User model definition */
+var userModel = new schema({
+    name: String,
+    email: { type: String, unique: true, lowercase: true },
+    password: String,
+    photo: String,
+    address_line1: { type: String, default: '' },
+    address_line2: { type: String, default: '' },
+    country_code: { type: String, default: '' },
+    postal_code: { type: String, default: '' },
+    tel: { type: String, default: '' }
+});
+
+/* Signup - hash user password before save to DB */
+userModel.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('password')) return next();
+    
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+        
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+/* Signup - generate user photo using Gravatar API */
+userModel.methods.generatePhoto = function() {
+    var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+    return 'https://gravatar.com/avatar/' + md5 + '?s=150&d=wavatar';
+};
+
+/* Login - validate user password */
+userModel.methods.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
+
+/* User model export */
+module.exports = mongoose.model('User', userModel);
